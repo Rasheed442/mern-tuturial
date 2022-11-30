03d09eafd230f2d28e3701df446a2324cf365a5d
Rasheed <abdulraufrasheed15@gmail.com>
2022-11-30 09:32:10 +0100

    init

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,39 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import React, {useState} from "react"
+import Axios from 'axios'
 export default function Home() {
+
+//  async function handler(e){
+//     e.preventDefault();
+//     const response = await fetch ("http//localhost:5000/Register",{
+//       method:"POST",
+//       head:{"Content-Type":"application/json"},
+//       body: JSON.stringify({
+//         name:name,
+//         age:age,
+//         username:username
+//       })
+      
+//     })
+//     const data = await response.json();
+//     if(error){
+//       console.log(error)
+//     }console.log(data)
+//   }
+   const handler = () => {
+      Axios.post("http://localhost:5000/Register", {
+        name,
+        age,
+        username,
+      }).then((response) =>{
+        alert("user created")
+      });
+   }
+  const [name, setName] = useState()
+  const [age, setAge] = useState()
+  const [username, setUsername] = useState()
   return (
     <div className={styles.container}>
       <Head>
@@ -11,61 +42,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <form onSubmit={handler}>  
+          <div className={styles.main}>
+             <input type='text' placeholder='NAME' onChange={(e)=> setName(e.target.value)}></input>
+             <input type='number' placeholder='AGE' onChange={(e)=> setAge(e.target.value)}></input>
+             <input type='text' placeholder='USERNAME' onChange={(e)=> setUsername(e.target.value)}></input>
+             <button type='submit'>SUBMIT</button>
+             </div>
+        </form>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
